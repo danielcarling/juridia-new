76f34a13f3cfff82f7a441557e4a2809a70aaa5d
Daniel Carling <117768647+danielcarling@users.noreply.github.com>
2023-10-28 08:31:50 -0400

    update

--- src/pages/payment/index.page.tsx
+++ src/pages/payment/index.page.tsx
@@ -2,9 +2,12 @@ import { RegisterAccountHeader } from "@/components/register-account/Header";
 import { CreditCardSvg } from "../../../public/payment/CreditCardIcon";
 import {
   Container,
+  FormContainer,
+  FormGroup,
   Main,
   PayOptionCard,
   PayOptionsContainer,
+  PaymentContainer,
   PaymentHeader,
   SalesArtContainer,
 } from "./styles";
@@ -24,36 +27,82 @@ export default function Payment() {
           <img src="/payment/salesArt.png" alt="" />
         </SalesArtContainer>
 
-        <PaymentHeader>
-          <strong>Realizar Assinatura</strong>
-          <span>Finalize sua Assinatura e comece a usar</span>
-        </PaymentHeader>
-
-        <PayOptionsContainer>
-          <PayOptionCard
-            selected={payOption === "pix"}
-            onClick={() => setPayOption("pix")}
-          >
-            <PixSvg />
-            <strong>PIX</strong>
-          </PayOptionCard>
-          <PayOptionCard
-            selected={payOption === "creditCard"}
-            onClick={() => setPayOption("creditCard")}
-          >
-            <CreditCardSvg />
-            <strong>Cartão</strong>
-          </PayOptionCard>
-        </PayOptionsContainer>
-
-        {payOption === "pix" && <div>URUBU DO PIX</div>}
-        {payOption === "creditCard" && (
-          <>
-            <div style={{ display: 'flex', justifyContent: 'center', margin: '1rem 0' }}>
-              <CreditCard />
-            </div>
-          </>
-        )}
+        <PaymentContainer>
+          <PaymentHeader>
+            <strong>Realizar Assinatura</strong>
+            <span>Finalize sua Assinatura e comece a usar</span>
+          </PaymentHeader>
+          <PayOptionsContainer>
+            <PayOptionCard
+              selected={payOption === "pix"}
+              onClick={() => setPayOption("pix")}
+            >
+              <PixSvg />
+              <strong>PIX</strong>
+            </PayOptionCard>
+            <PayOptionCard
+              selected={payOption === "creditCard"}
+              onClick={() => setPayOption("creditCard")}
+            >
+              <CreditCardSvg />
+              <strong>Cartão</strong>
+            </PayOptionCard>
+          </PayOptionsContainer>
+          {payOption === "pix" && <div>URUBU DO PIX</div>}
+          {payOption === "creditCard" && (
+            <>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  margin: "1rem 0",
+                }}
+              >
+                <CreditCard />
+              </div>
+              <FormContainer>
+                <FormGroup>
+                  <label htmlFor="name">Nome do Cartão</label>
+                  <input
+                    type="text"
+                    id="cardName"
+                    placeholder="Digite o nome do cartão"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <label htmlFor="cardNumber">Número do Cartao</label>
+                  <input
+                    type="text"
+                    id="cardNumber"
+                    placeholder="Digite o número do cartao"
+                  />
+                </FormGroup>
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <FormGroup className="validityAndCVC">
+                    <label htmlFor="validity">Data de vencimento</label>
+                    <input
+                      type="text"
+                      id="validity"
+                      placeholder="MM/AA"
+                      className="validity"
+                    />
+                  </FormGroup>
+                  <FormGroup className="validityAndCVC">
+                    <label htmlFor="securityCode">CVC</label>
+                    <input
+                      type="text"
+                      id="securityCode"
+                      placeholder="XXX"
+                      className="securityCode"
+                    />
+                  </FormGroup>
+                </div>
+              </FormContainer>
+            </>
+          )}
+        </PaymentContainer>
       </Main>
       <Footer />
     </Container>
--- src/pages/payment/styles.ts
+++ src/pages/payment/styles.ts
@@ -8,7 +8,32 @@ export const Container = styled.div`
   font-family: "Archivo", sans-serif;
 `;
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 1024px) {
+    flex-direction: row-reverse;
+  }
+`;
+
+export const SalesArtContainer = styled.div`
+  margin: auto;
+  padding: 0.75rem;
+
+  img {
+    width: 100%;
+    width: clamp(100%, 100%, 30rem);
+  }
+`;
+
+export const PaymentContainer = styled.div`
+  width: 100%;
+
+  @media (min-width: 1024px) {
+    width: 50vw;
+  }
+`;
 
 interface ProgressBarProps {
   step: number;
@@ -27,14 +52,6 @@ export const ProgressBar = styled.div<ProgressBarProps>`
 
   @media (min-width: 1024px) {
     top: -0.125rem;
-  }
-`;
-
-export const SalesArtContainer = styled.div`
-  padding: 0.75rem;
-
-  img {
-    width: 100%;
   }
 `;
 
@@ -89,5 +106,40 @@ export const PayOptionCard = styled.div<PayOptionCardProps>`
     margin-left: 1rem;
     width: 3.5rem;
     height: auto;
+  }
+`;
+
+export const FormContainer = styled.div`
+  padding: 0 2rem;
+  max-width: 26.25rem;
+  margin: auto;
+`;
+
+export const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2vh;
+
+  & label {
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+
+  & input {
+    padding: 1rem;
+    border: 1px solid #d7d9dd;
+    background-color: transparent;
+    border-radius: 5px;
+    transition: 0.3s;
+    color: ${({ theme }) => theme.color.gray_20};
+
+    &::placeholder {
+      color: ${({ theme }) => theme.color.gray_80};
+    }
+  }
+
+  .validity,
+  .securityCode {
+    width: 8rem;
   }
 `;
