3cda62a8ab0cdb0935f8a10c4c4dfe689db4ebd3
Daniel Carling <117768647+danielcarling@users.noreply.github.com>
2023-11-07 10:10:20 -0400

    Update creditCardValidation.js

--- src/utils/creditCardValidation.js
+++ src/utils/creditCardValidation.js
@@ -1,4 +1,4 @@
-import moment from "moment";
+// import moment from "moment";
 
 export function stripeCardNumberValidation(cardNumber) {
   const regexPattern = {
@@ -24,61 +24,61 @@ export function stripeCardNumberValidation(cardNumber) {
   return "Insira um cartão válido";
 }
 
-export const stripeCardExpirValidation = (value) => {
-  if (value) {
-    if (/^(0[1-9]|1[0-2])\/[0-9]{2}$/i.test(value.trim())) {
-      let today = new Date();
-      let CurrentDate = moment(
-        new Date(
-          today.getFullYear() +
-          "-" +
-          (today.getMonth() + 1) +
-          "-" +
-          new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()
-        )
-      );
-      let visaValue = value.split("/");
-      let visaDate = new Date(`20${visaValue[1]}`, visaValue[0], 0);
-      return CurrentDate < moment(visaDate)
-        ? undefined
-        : "Insira uma data válida";
-    } else {
-      return "Formato de data inválido";
-    }
-  }
-};
+// export const stripeCardExpirValidation = (value) => {
+//   if (value) {
+//     if (/^(0[1-9]|1[0-2])\/[0-9]{2}$/i.test(value.trim())) {
+//       let today = new Date();
+//       let CurrentDate = moment(
+//         new Date(
+//           today.getFullYear() +
+//           "-" +
+//           (today.getMonth() + 1) +
+//           "-" +
+//           new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()
+//         )
+//       );
+//       let visaValue = value.split("/");
+//       let visaDate = new Date(`20${visaValue[1]}`, visaValue[0], 0);
+//       return CurrentDate < moment(visaDate)
+//         ? undefined
+//         : "Insira uma data válida";
+//     } else {
+//       return "Formato de data inválido";
+//     }
+//   }
+// };
 
-export const textWithSpacesOnly = (value) => {
-  if (value) {
-    if (/^[a-zA-Z ]*$/i.test(value)) {
-      return undefined;
-    } else {
-      return "Somente letras";
-    }
-  } else {
-    return undefined;
-  }
-};
+// export const textWithSpacesOnly = (value) => {
+//   if (value) {
+//     if (/^[a-zA-Z ]*$/i.test(value)) {
+//       return undefined;
+//     } else {
+//       return "Somente letras";
+//     }
+//   } else {
+//     return undefined;
+//   }
+// };
 
-export const minLength = (min) => (value) =>
-  value && value.length < min ? `Deve ter pelo menos ${min} dígitos` : undefined;
-
-
+// export const minLength = (min) => (value) =>
+//   value && value.length < min ? `Deve ter pelo menos ${min} dígitos` : undefined;
 
 
-export const fieldNameValidation = (data) => {
+
+
+// export const fieldNameValidation = (data) => {
   
-  if(data.creditCard.number === ''){return 'Número do cartão é obrigatório'}
-  if(data.creditCard.holderName === ''){return 'Nome do cartão é obrigatório'}
-  if(data.creditCard.ccv === ''){return 'Código de segurança é obrigatório'}
-  if(data.creditCard.expiryMonth === ''){return 'expiração do cartão é obrigatória'}
+//   if(data.creditCard.number === ''){return 'Número do cartão é obrigatório'}
+//   if(data.creditCard.holderName === ''){return 'Nome do cartão é obrigatório'}
+//   if(data.creditCard.ccv === ''){return 'Código de segurança é obrigatório'}
+//   if(data.creditCard.expiryMonth === ''){return 'expiração do cartão é obrigatória'}
 
-  if(data.creditCardHolderInfo.name === ''){return 'Nome completo é obrigatório'}
-  if(data.creditCardHolderInfo.cpfCnpj === ''){return 'CPF ou Cnpj é obrigatório'}
-  if(data.creditCardHolderInfo.postalCode === ''){return 'CEP é obrigatório'}
-  if(data.creditCardHolderInfo.addressNumber === ''){return 'Número é obrigatório'}
-  if(data.creditCardHolderInfo.phone === ''){return 'Telefone é obrigatório'}
-  if(!data.numInstallment){return 'Número de parcelas é obrigatório'}
+//   if(data.creditCardHolderInfo.name === ''){return 'Nome completo é obrigatório'}
+//   if(data.creditCardHolderInfo.cpfCnpj === ''){return 'CPF ou Cnpj é obrigatório'}
+//   if(data.creditCardHolderInfo.postalCode === ''){return 'CEP é obrigatório'}
+//   if(data.creditCardHolderInfo.addressNumber === ''){return 'Número é obrigatório'}
+//   if(data.creditCardHolderInfo.phone === ''){return 'Telefone é obrigatório'}
+//   if(!data.numInstallment){return 'Número de parcelas é obrigatório'}
 
-  return 'ok'
-}
+//   return 'ok'
+// }
